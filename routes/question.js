8753d124988fd7ea7d4bfcee9228fecd5eecbabc
Drew Ahrens <drew.ahrens@gmail.com>
2021-10-12 14:01:41 -0400

    Initial attempt on working the backend.

--- routes/question.js
+++ routes/question.js
@@ -1,15 +1,39 @@
-const dataPath = "./data/question.json";
+const dataPath = "./data/question.json"
+var fs = require('fs').promises
 
-const questionRoutes = (app, fs) => {
-  app.get("/questions", (req, res) => {
-    fs.readFile(dataPath, "utf8", (err, data) => {
-      if (err) {
-        throw err
-      }
-      res.send(JSON.parse(data))
-    })
+
+const questionRoutes = (app) => {
+  app.get("/questions", async (req, res) => {
+    let data = await fs.readFile(dataPath, "utf8")
+      .catch(err => { throw new Error(err)})
+    console.log(data)
+    res.send(JSON.parse(data))
   })
-
+  // Post for creating new questions
+  app.post("/questions", async (req, res) => {
+    // get the request body
+    let data = JSON.parse(req.body)
+    // read the questions.json file into the correct object
+    let questions = await fs.readFile(dataPath, "utf8")
+    questions.push(data)
+    await fs.writeFileSync(dataPath, JSON.stringify(questions))
+      .then(() => {
+        req.send(JSON.parse("{ 'success': true }"))
+      })
+      .catch(err => { throw new Error(err)})
+  })
+  // 
+  app.patch("/questions", async (req, res) => {
+    let data = JSON.parse(req.body)
+    let questions = await fs.readFile(dataPath, "utf8")
+    questions.splice(data.id, 1)
+    questions.push(data.question)
+    await fs.writeFileSync(dataPath, JSON.stringify(questions))
+      .then(() => {
+        req.send(JSON.parse("{ 'success': true }"))
+      })
+      .catch(err => { throw new Error(err)})
+  })
   /*app.get("/lbcf/:id", (req, res) => {
     fs.readFile(dataPath, "utf8", (err, data) => {
       if (err) { throw err }
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,14 @@
 const path = require('path')
 const questionRoutes = require("./question")
 
-const appRouter = (app, fs) => {
+const appRouter = (app) => {
   // we've added in a default route here that handles empty routes
   // at the base API url
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/../public/index.html'))
   })
 
-  questionRoutes(app, fs)
+  questionRoutes(app)
 }
 
 module.exports = appRouter
